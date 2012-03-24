b270f13fbb3e8cb73fd2384a4134a152e85eb63f
Miel VanderSande <miel.vandersande@ugent.be>
2012-03-24 11:54:10 +0100

    scripts added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,29 @@
 
 */
 
+getLokalen();
 
+function showLokalen(data){
+    var div = $('<div />');
+    div.appendTo($("#content"));
+        div.attr("data-role", "controlgroup");
+    
+    for (var lokaal in data.jeugdwerk){
+//        <div data-role="controlgroup">
+//<a href="index.html" data-role="button">Yes</a>
+//<a href="index.html" data-role="button">No</a>
+//<a href="index.html" data-role="button">Maybe</a>
+//</div>
+
+        $("<a />").attr("href", "")
+        .attr("data-role","button")
+        .text(lokaal.organisati)
+        .appendTo(div);
+        
+        
+        
+    }
+}
 
 
 
